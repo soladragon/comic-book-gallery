70334bc97a04d7dc097349cd4f2feac2acb20628
johncraigmurphy@gmail.com <johncraigmurphy@gmail.com>
2019-02-07 17:43:58 +0000

    Update app class

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -7,6 +7,20 @@ using System.Web.Mvc;
 namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
-    {
+    { 
+        //Action method Detail()
+        public ContentResult Detail()
+        {
+            //ContentResult() is an MVC action result type
+            return new ContentResult()
+            {
+                if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return new RedirectResult("/");
+            }
+                Content = "Hello from the comic book controller!"
+            };
+           
+        }
     }
 }
